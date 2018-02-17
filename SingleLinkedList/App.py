c3b8bb94ae9d75ee54e435769928206417a76f41
Enigma <write2diba@gmail.com>
2018-02-17 13:23:12 +0530

    Add files via upload

---
+++ SingleLinkedList/App.py
@@ -0,0 +1,11 @@
+from LinkedList import LinkedList
+linkedlist=LinkedList()
+linkedlist.insertStart(12)
+linkedlist.insertEnd(122)
+linkedlist.insertEnd(3)
+linkedlist.insertEnd(121)
+
+linkedlist.traverseList()
+
+linkedlist.remove(3)
+linkedlist.traverseList()
